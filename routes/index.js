94ffe04ed97a32e37a8f47daaad6abb2ea057b14
Hannan Ali <abdulhannanali@users.noreply.com>
2021-10-09 12:19:04 +0500

    add comments in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const { Readable } = require('stream')
+
+// Replace this fakeGetCustomers call with an original one, as long as it's returning an iterable that can be converted
+// into a stream
+// this logic would work without errors
 const getCustomers = require('../fakeGetCustomers')
 
 const router = express.Router()
@@ -8,6 +12,11 @@ function stringifyCustomer (customer) {
   return JSON.stringify(customer)
 }
 
+/**
+ *
+ *
+ * @returns {Readable} returns a readable stream that can be piped into res
+ */
 async function getCustomersReadableStream () {
   return Readable.from((await getCustomers()).map((customer) => `${stringifyCustomer(customer)}--record-finish--`))
 }
